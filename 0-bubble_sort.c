8744a66d0b6998c55bef67a6afb85f622ae4354b
Hedgebzh <axel.gore35@gmail.com>
2022-08-22 15:36:15 +0200

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
 
-	if (array == '\0' || size == '\0')
+	if (array == NULL || size == 0)
 	return;
 
     for (i = 0; i < size - 1; i++)
