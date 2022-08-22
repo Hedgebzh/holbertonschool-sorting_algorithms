c814231b405bace1b7dc39528472d334cf626662
Hedgebzh <axel.gore35@gmail.com>
2022-08-22 15:30:45 +0200

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
 
+	if (array == NULL || size == NULL)
+	return (0);
+
     for (i = 0; i < size - 1; i++)
 
         // Last i elements are already in place
