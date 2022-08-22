67451cb7783bbb8faa251d30b5c26f7b2978bb19
Hedgebzh <axel.gore35@gmail.com>
2022-08-22 15:39:07 +0200

    fix task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,27 @@
 #include "sort.h"
 
-void swap(int* xp, int* yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+	int tmp;
+	size_t i, j;
 
 	if (array == NULL || size == 0)
-	return;
+		return;
 
-    for (i = 0; i < size; i++)
-
-        for (j = 0; j < size - 1; j++)
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
 		{
-            if (array[j] > array[j + 1])
-                swap(&array[j], &array[j + 1]);
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
+			}
 		}
+	}
 }
-
 
 
 
